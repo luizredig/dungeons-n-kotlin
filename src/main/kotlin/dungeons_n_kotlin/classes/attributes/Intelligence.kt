e6ca4934a6a86c87644399b3f25da79b22f6ace9
redig <luizredig@gmail.com>
2024-08-15 16:29:56 -0300

    feat: add the first six player attributes

---
+++ src/main/kotlin/dungeons_n_kotlin/classes/attributes/Intelligence.kt
@@ -0,0 +1,11 @@
+package dungeons_n_kotlin.classes.attributes
+
+import dungeons_n_kotlin.interfaces.attributes.Attribute
+
+class Intelligence(
+    override var name: String,
+    override var value: Int,
+    override var raceBonus: Int?,
+    override var classBonus: Int?
+) : Attribute {
+}
