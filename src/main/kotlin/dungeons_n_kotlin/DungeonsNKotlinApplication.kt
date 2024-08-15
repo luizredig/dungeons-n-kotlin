097bffad3ff6daf9f0ff69a9af93d07174213d6e
redig <luizredig@gmail.com>
2024-08-15 11:30:11 -0300

    chore(init): setup Spring Boot project with Kotlin support

---
+++ src/main/kotlin/dungeons_n_kotlin/DungeonsNKotlinApplication.kt
@@ -0,0 +1,11 @@
+package dungeons_n_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DungeonsNKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<DungeonsNKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/dungeons_n_kotlin/DungeonsNKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package dungeons_n_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DungeonsNKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
