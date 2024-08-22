9a03aed7c8640562350e7dc9c38cf59b1f49a482
redig <luizredig@gmail.com>
2024-08-22 14:02:18 -0300

    refactor(attributes): delete all attributes classes

--- src/main/kotlin/dungeons_n_kotlin/classes/attributes/Intelligence.kt
+++
@@ -1,8 +0,0 @@
-package dungeons_n_kotlin.classes.attributes
-
-import dungeons_n_kotlin.interfaces.attributes.Attribute
-
-class Intelligence(
-    override var name: String,
-    override var value: Int
-) : Attribute
--- src/main/kotlin/dungeons_n_kotlin/classes/attributes/Strength.kt
+++
@@ -1,8 +0,0 @@
-package dungeons_n_kotlin.classes.attributes
-
-import dungeons_n_kotlin.interfaces.attributes.Attribute
-
-class Strength(
-    override var name: String,
-    override var value: Int
-) : Attribute
