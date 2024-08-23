ab2fd247ab339ae5819592a444e509974b1e8011
Vithugsil <vitorhugosilva012@gmail.com>
2024-08-22 21:48:00 -0300

    feat(main): init application menu

--- src/main/kotlin/dungeons_n_kotlin/DungeonsNKotlinApplication.kt
+++ src/main/kotlin/dungeons_n_kotlin/DungeonsNKotlinApplication.kt
@@ -7,5 +7,24 @@ import org.springframework.boot.runApplication
 class DungeonsNKotlinApplication
 
 fun main(args: Array<String>) {
-	runApplication<DungeonsNKotlinApplication>(*args)
+//	runApplication<DungeonsNKotlinApplication>(*args)
+
+	var isGameRunning: Boolean = true
+	do {
+
+		println("Selecione uma opção:")
+		println("(1) Criar personagem \n(2) Sair")
+		var response = readLine()
+		when(response){
+			"1" -> println("AAAAAA")
+			"2" -> isGameRunning = false
+			else -> throw error("Invalid response")
+		}
+
+	}while (isGameRunning)
+
 }
+
+//Fazer menu com duas opções criar personagem ou sair
+//definir nome, raça e atributo -> criar personagens | Distribuição de 27 pontos nos atributos
+
