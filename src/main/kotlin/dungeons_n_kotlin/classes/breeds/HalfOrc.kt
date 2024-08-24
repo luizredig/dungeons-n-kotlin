b9e85281aa4712b902a877301b9b5f637d590ec1
Thiago Bernardino <thiagobernardino6@gmail.com>
2024-08-24 17:57:51 -0300

    feat(breed): create HalfOrc class

---
+++ src/main/kotlin/dungeons_n_kotlin/classes/breeds/HalfOrc.kt
@@ -0,0 +1,30 @@
+package dungeons_n_kotlin.classes.breeds
+
+import dungeons_n_kotlin.interfaces.breed.IBreed
+
+class HalfOrc  : IBreed {
+    override fun getStrength(): Int {
+        return 2
+    }
+
+    override fun getDexterity(): Int {
+        return 0
+    }
+
+    override fun getConstitution(): Int {
+        return 1
+    }
+
+    override fun getIntelligence(): Int {
+        return 0
+    }
+
+    override fun getWisdom(): Int {
+        return 0
+    }
+
+    override fun getCharisma(): Int {
+        return 0
+    }
+
+}
