ce64b7e2e915c7a70d068c8a1c91c78d3ff8e586
redig <luizredig@gmail.com>
2024-08-25 02:05:02 -0300

    feat(junit): add test to ensure that the minimum attribute value is 8

--- src/test/kotlin/dungeons_n_kotlin/DungeonsNKotlinApplicationTests.kt
+++ src/test/kotlin/dungeons_n_kotlin/DungeonsNKotlinApplicationTests.kt
@@ -1,13 +1,24 @@
 package dungeons_n_kotlin
 
+import dungeons_n_kotlin.classes.attributes.Attributes
+import dungeons_n_kotlin.classes.entities.Character
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import kotlin.test.assertEquals
 
 @SpringBootTest
 class DungeonsNKotlinApplicationTests {
+    private val characterTester = Character()
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun `ensure that the minimum attribute value is 8`() {
+        characterTester.attributes = Attributes()
 
+        assertEquals(8, characterTester.attributes.strength)
+        assertEquals(8, characterTester.attributes.dexterity)
+        assertEquals(8, characterTester.attributes.constitution)
+        assertEquals(8, characterTester.attributes.intelligence)
+        assertEquals(8, characterTester.attributes.wisdom)
+        assertEquals(8, characterTester.attributes.charisma)
+    }
 }
